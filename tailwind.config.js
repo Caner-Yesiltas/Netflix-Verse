84afe2609cb76915b5e005396e3772748fb2f769
CanerYesiltas <caneryesiltas1@gmail.com>
2025-03-09 17:33:33 -0400

    Add icons and images to public folder, update global styles, and extend theme colors in Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "gray-dark-main": "#23242a",
+        "gray-dark-second": "#28292d",
+        "gray-light": "#d3dce6",
+        "red-main": "#ff4b45",
+      },
+    },
   },
   plugins: [],
 };
