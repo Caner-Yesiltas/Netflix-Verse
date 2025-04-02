f9531834c0994e93af1f60ae28f33c405dda56eb
CanerYesiltas <caneryesiltas1@gmail.com>
2025-04-02 11:58:17 -0400

    feat: add getVideoKey async function & movie title, overview, and id props to HeroSection

--- src/app/(private)/movies/page.jsx
+++ src/app/(private)/movies/page.jsx
@@ -1,14 +1,20 @@
 import { getMovies } from '@/helpers/movieFunctions';
 import React from 'react'
+import HeroSection from './components/Herosection';
 export const metadata = {
   title: "Movies Page",
 };
 
 const Movies = async () => {
   const movies = await getMovies("now_playing")
-  console.log(movies)
+  console.log(movies);
   return (
     <div>
+      <HeroSection
+      title ={movies[0].title}
+      overview={movies[0]?.overview}
+      id={movies[0]?.id}
+      />
     </div>
   )
 }
--- src/helpers/movieFunctions.js
+++ src/helpers/movieFunctions.js
@@ -1,24 +1,29 @@
-const API_KEY =process.env.TMDB_KEY;
-
-
+const API_KEY = process.env.TMDB_KEY;
 
 export const getMovies = async (type) => {
-    const URL = `https://api.themoviedb.org/3/movie/${type}?api_key=${API_KEY}&language=en-US&page=1`;
-    const res = await fetch(URL)
+  const URL = `https://api.themoviedb.org/3/movie/${type}?api_key=${API_KEY}&language=en-US&page=1`;
+  const res = await fetch(URL);
 
-   //   const res = await fetch(URL, { cache: "force-cache" }); default
-   //   const res = await fetch(URL, { cache: "no-store" });
-   //   const res = await fetch(URL, { next: { revalidate: 10 } });
+  //   const res = await fetch(URL, { cache: "force-cache" }); default
+  //   const res = await fetch(URL, { cache: "no-store" });
+  //   const res = await fetch(URL, { next: { revalidate: 10 } });
 
-    if (!res.ok) {
-      
-      throw new Error('Failed to fetch data')
-    }
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
 
-    const results = await res.json()
-    console.log(results.results)
-    return results;
-}
+  const { results } = await res.json();
+  return results;
+};
 
+export const getVideoKey = async (id) => {
+  const URL = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}&language=en-US`;
+  const res = await fetch(URL);
 
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
 
+  const results = await res.json();
+  return results.results[0].key;
+};
