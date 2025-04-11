d399d09b084c8bb23bf5cccfe0509d324dc82bed
CanerYesiltas <caneryesiltas1@gmail.com>
2025-04-11 12:42:56 -0400

    feat: add language param & revalidate to movie detail api [use en-US to avoid localization issues revalidate set to 86400 (1 day) for caching]

--- src/helpers/movieFunctions.js
+++ src/helpers/movieFunctions.js
@@ -17,13 +17,28 @@ export const getMovies = async (type) => {
 };
 
 export const getVideoKey = async (id) => {
-  const URL = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}&language=en-US`;
-  const res = await fetch(URL);
+  const vıdeoUrl = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${API_KEY}&language=en-US`;
+  const res = await fetch(vıdeoUrl, { next: { revalidate: 86400 } });
 
   if (!res.ok) {
     throw new Error('Failed to fetch data');
   }
 
   const results = await res.json();
-  return results.results[0].key;
+  return results.results[0]?.key;
 };
+
+
+export const getMovieDetails = async (movieId) => {
+  const lang = "en-US"; // ensure API responses return English content consistently
+const movieDetailBaseUrl= `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=${lang}`;
+
+const res = await fetch(movieDetailBaseUrl, { next: { revalidate: 86400 } });  // revalidate data every 24 hours (ISR)
+
+if (!res.ok) {
+  throw new Error("Failed to fetch data");
+}
+
+const data = await res.json()
+return data;
+}
